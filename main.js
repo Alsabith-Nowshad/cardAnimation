63a95d7a5aac08aa2690b560525b3dcc503c52b2
Alsabith-Nowshad <alsabithnowshad123@gmail.com>
2024-07-07 21:24:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,25 @@
 let box1 = document.querySelector(".box1")
 let box2 = document.querySelector(".box2")
 let box3 = document.querySelector(".box3")
-let content = document.querySelector(".content")
+let content1 = document.querySelector(".content1")
 let titleDiv = document.querySelector(".titleDiv")
 
 box1.addEventListener("mouseover",function(){
 
 box1.style.width = "30vw"
+
 titleDiv.style.display = "none"
-content.style.display = "block"
+content1.style.display = "block"
+content1.style.transitionDelay = "2s"
 
 })
 
 box1.addEventListener("mouseleave",function(){
 
     box1.style.width = "10vw"
-
 titleDiv.style.display = "block"
-content.style.display = "none"
+content1.style.display = "none"
     
     })
     
-
-box2.addEventListener("mouseover",function(){
-
-    box2.style.width = "30vw"
-        
-})
-        
-        box2.addEventListener("mouseleave",function(){
-        
-            box2.style.width = "10vw"
-            
-            })
-    
-            
-            box3.addEventListener("mouseover",function(){
-
-                box3.style.width = "30vw"
-                
-                })
-                
-                box3.addEventListener("mouseleave",function(){
-                
-                    box3.style.width = "10vw"
-                    
-                    })
-                    
-
 
